d674332652a1fdaf92f2b644b2764c73d4569e9a
Georgy <whysalakh@gmail.com>
2026-05-28 10:12:58 +0200

    Add weighted sampling MIS algorithm

--- src/mis.h
+++ src/mis.h
@@ -254,6 +254,83 @@ NodeList weighted_greedy_mis(const GraphT& g) {
     return mis;
 }
 
+// approach 2: weighted luby 
+// we have random priorty using weighted random sampling
+// random priority = uniform^(1/weight), thus higher wight= higher priority
+template <typename GraphT>
+NodeList weighted_sampling_mis(const GraphT& g) {
+    const auto n = g.size();
+    if (n == 0) return {};
+
+    std::vector<uint8_t> is_active(n, 1); 
+    std::vector<double> priorities(n, 0.0);
+    NodeList mis;
+
+    bool any_active = true;
+    while (any_active) {
+        #pragma omp parallel
+        {
+            static thread_local std::mt19937 gen(std::random_device{}());
+            std::uniform_real_distribution<double> dist(0.0, 1.0);
+            #pragma omp for
+            for (std::size_t node = 0; node < n; node++) {
+                if (is_active[node])
+                    priorities[node] = std::pow(dist(gen), 1.0 / g.weight(node));
+            }
+        }
+
+        std::vector<uint32_t> newly_added;
+        #pragma omp parallel
+        {
+            std::vector<uint32_t> local_newly_added;
+            #pragma omp for
+            for (std::size_t node = 0; node < n; node++) {
+                if (!is_active[node])
+                    continue;
+
+                bool is_max = true;
+                for (auto son : g[node]) {
+                    if (is_active[son]) {
+                        if (priorities[son] > priorities[node] || (priorities[son] == priorities[node] && son > node)) {
+                            is_max = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (is_max) {
+                    local_newly_added.push_back(node);
+                }
+            }
+            #pragma omp critical
+            {
+                newly_added.insert(newly_added.end(), local_newly_added.begin(), local_newly_added.end());
+            }
+        }
+
+        for (auto node : newly_added) {
+            mis.push_back(node);
+            is_active[node] = 0;
+            for (auto son : g[node]) {
+                is_active[son] = 0;
+            }
+        }
+
+        any_active = false;
+        #pragma omp parallel for reduction(||:any_active)
+        for (std::size_t i = 0; i < n; i++) {
+            if (is_active[i]) {
+                any_active = true;
+            }
+        }
+    }
+
+    return mis;
+}
+
+// approach 3: weighted sampling improved
+
+
 enum class Algorithm {
     Sequential,
     Luby,
